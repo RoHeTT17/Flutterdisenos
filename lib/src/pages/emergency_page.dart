9c135592ee35289cdcba27c782d8ac025e4a8c59
rhernandezr <rhernandezr@jumapacelaya.gob.mx>
2022-06-23 11:34:58 -0500

    Fin seccion 7

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-import 'package:disenos/src/widgets/headers.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:disenos/src/widgets/headers.dart';
+import 'package:disenos/src/widgets/boton_gordo.dart';
+
+class ItemBoton {
+
+  final IconData icon;
+  final String texto;
+  final Color color1;
+  final Color color2;
+
+  ItemBoton( this.icon, this.texto, this.color1, this.color2 );
+}
+
+
 
 
 class EmergencyPage extends StatelessWidget {
@@ -10,14 +25,109 @@ class EmergencyPage extends StatelessWidget {
  
   @override
   Widget build(BuildContext context) {
+
+  final items = <ItemBoton>[
+    new ItemBoton( FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5), Color(0xff906EF5) ),
+    new ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2), Color(0xff536CF6) ),
+    new ItemBoton( FontAwesomeIcons.theaterMasks, 'Theft / Harrasement', Color(0xffF2D572), Color(0xffE06AA3) ),
+    new ItemBoton( FontAwesomeIcons.biking, 'Awards', Color(0xff317183), Color(0xff46997D) ),
+    new ItemBoton( FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5), Color(0xff906EF5) ),
+    new ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2), Color(0xff536CF6) ),
+    new ItemBoton( FontAwesomeIcons.theaterMasks, 'Theft / Harrasement', Color(0xffF2D572), Color(0xffE06AA3) ),
+    new ItemBoton( FontAwesomeIcons.biking, 'Awards', Color(0xff317183), Color(0xff46997D) ),
+    new ItemBoton( FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5), Color(0xff906EF5) ),
+    new ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2), Color(0xff536CF6) ),
+    new ItemBoton( FontAwesomeIcons.theaterMasks, 'Theft / Harrasement', Color(0xffF2D572), Color(0xffE06AA3) ),
+    new ItemBoton( FontAwesomeIcons.biking, 'Awards', Color(0xff317183), Color(0xff46997D) ),
+  ];
+
+    //Convertir un listado de items a un listado de Widgets
+    List<Widget> itemMap = items.map(
+      (e) => FadeInLeft(
+        //duration: const Duration(milliseconds: 1500),
+        child: BotonGordo(icon: e.icon,texto: e.texto,color1: e.color1,color2: e.color2 ,onPress: (){print('Holis');}))
+      ).toList();
+
     return Scaffold(
-      body: IconHeader(
-                        icon: FontAwesomeIcons.plus, 
-                        titulo: "Asistencia Médica",
-                        subTitulo: "Haz Solicitado",
-                        color1: Color(0xff526BF6),
-                        color2: Color(0xff67ACF2),
-                      )
+      //backgroundColor: Colors.red, //solo es para validar que el icono no salga del botón
+      body: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 200),
+            child: ListView(
+                    physics: BouncingScrollPhysics(),
+                    children: [
+                          const SizedBox(height: 80,),
+                          ...itemMap //desplegar todo el listado
+                    ],
+            ),
+          ),
+
+          //Entre mas abajo se esta en el código significa que esta mas arriba en la pantalla
+          const _Header()
+        ],
+      )
    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        IconHeader(
+                    icon: FontAwesomeIcons.plus, 
+                    titulo: 'Asistencia Médica', 
+                    subTitulo: 'Haz solicitado',
+                    color1: const Color(0xff536CF6),
+                    color2: const Color(0xff66A9F2), 
+        ),
+
+        //Agregar botón menú
+        Positioned(
+          right: 0,
+          top: 45,
+          child: RawMaterialButton(
+             shape:   const CircleBorder(), //Al dar Tab sobre el botón, muestra un circulo al rededor
+             padding: const EdgeInsets.all(15),
+             child:   const FaIcon(FontAwesomeIcons.ellipsisVertical,color: Colors.white,),
+             onPressed: () {  },
+            )
+       )
+      ],
+    );
+  }
+}
+
+class BotonGordoTemp extends StatelessWidget {
+  const BotonGordoTemp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BotonGordo(texto: 'Motor Accident',icon: FontAwesomeIcons.carBurst,color1: Color(0xff6989F5), color2: Color(0xff906EF5),onPress: (){print("Holis");},);
+  }
+}
+
+class PageHeader extends StatelessWidget {
+  const PageHeader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconHeader(
+                      icon: FontAwesomeIcons.plus, 
+                      titulo: "Asistencia Médica",
+                      subTitulo: "Haz Solicitado",
+                      color1: const Color(0xff526BF6),
+                      color2: const Color(0xff67ACF2),
+                    );
   }
 }
---
+++ lib/src/widgets/boton_gordo.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BotonGordo extends StatelessWidget {
+
+  final IconData? icon;
+  final String texto;
+  final Color? color1;
+  final Color? color2;
+  final Function() onPress;
+
+
+  BotonGordo({Key? key, this.icon = FontAwesomeIcons.aws, required this.texto, this.color1 = Colors.grey, this.color2 = Colors.blueGrey, required this.onPress}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPress,
+      child: Stack(
+        children: [
+          _BackGround(icon: this.icon!,color1:this.color1! ,color2: this.color2!,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 140, width: 40,),
+              FaIcon(icon,size: 40,color: Colors.white),
+              const SizedBox(width: 20,),
+              Expanded(child: Text(texto, style: const TextStyle(color: Colors.white, fontSize: 18),)),
+              const FaIcon(FontAwesomeIcons.chevronRight,size: 40,color: Colors.white),
+              const SizedBox(width: 40,)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _BackGround extends StatelessWidget {
+  
+  final IconData icon;
+  final Color color1;
+  final Color color2;
+  
+  const _BackGround({Key? key, required this.color1, required this.color2, required this.icon,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       width: double.infinity,
+       height: 100,
+       margin: const EdgeInsets.all(20),
+       decoration: BoxDecoration(
+                            //color: Colors.red, 
+                            boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black.withOpacity(0.2),
+                                      offset: Offset(4, 6),
+                                      blurRadius: 10,
+                                    ),
+                            ],
+                            borderRadius: BorderRadius.circular(15),
+                            gradient: LinearGradient(
+                              colors: [
+                                        color1,
+                                        color2,
+                                      ],
+                            )
+       ),
+       child: ClipRRect(
+         borderRadius: BorderRadius.circular(15),
+         child: Stack(
+           children: [
+              Positioned(
+                         right: -20,
+                         top:   -20, 
+                         child: FaIcon(icon,size: 150,color: Colors.white.withOpacity(0.2),)
+                        )
+           ],
+         ),
+       ), 
+
+
+    );
+  }
+}
